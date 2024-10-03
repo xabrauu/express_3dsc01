1a04b8d4b3d4764f877970425cbdc256555dc476
bruno <130366440+xabrauu@users.noreply.github.com>
2024-10-03 11:29:33 +0000

    criado a rota home e sobre

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 const PORT = 3301;
 
 app.get("/", function (req, res) {
-  res.send("Olá!....");
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
 });
 
 app.listen(PORT, () => {
