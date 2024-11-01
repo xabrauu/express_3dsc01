d720482b848141f871dcd8457d494bd0098795e0
bruno <130366440+xabrauu@users.noreply.github.com>
2024-11-01 15:16:34 +0000

    Ajustes no Menu, Rotas e CSS

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const app = express();
 
 const PORT = 3301;
 
+//configurando rotas de estaticos
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/src/views/index.html");
 });
 
 app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+
+app.get("/contato", function (req, res) {
   res.sendFile(__dirname + "/src/views/about.html");
 });
 
